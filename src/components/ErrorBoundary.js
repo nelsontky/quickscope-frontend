10f73b686a4c45731334155321457cba2a59adb5
nelsontky <nelson@ccb.wtf>
2020-11-06 14:44:58 +0800

    Improve errorboundary

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
 
 export default class ErrorBoundary extends React.Component {
   state = { hasError: false, error: null };
@@ -15,11 +16,21 @@ export default class ErrorBoundary extends React.Component {
     if (this.state.hasError) {
       return (
         <Container maxWidth="sm">
-          <Typography variant="h5" gutterBottom>Page not found</Typography>
-          <Typography variant="body1">
-            Oops! The page you are looking for does not exist! Please refresh
-            your browser.
+          <Typography variant="h5" gutterBottom>
+            Page not found
           </Typography>
+          <Typography variant="body1" gutterBottom>
+            Oops! The page you are looking for does not exist!
+          </Typography>
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={() => {
+              window.location.href = process.env.PUBLIC_URL;
+            }}
+          >
+            Go back to homepage
+          </Button>
         </Container>
       );
     }
